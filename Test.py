bf2f8b27f80a2d4adfc56ed83283bfe676af6125
anhnguyen304 <hoanganh9304@gmail.com>
2022-06-13 20:27:49 -0700

    change filter

--- Test.py
+++ Test.py
@@ -1,33 +1,44 @@
-from token import LEFTSHIFT
 import cv2
-from cv2 import imread 
 import face_recognition
 import  cvzone
 import numpy as np
-import random, time
 
-def sepiaFilter (image):
+def sepiaFilter (frame):
     # Convert to float to prevent loss
-    sepiaFilter = np.array(image, dtype = np.float64)
-    sepiaFilter = cv2.transform(sepiaFilter, np.matrix([[0.272, 0.543, 0.131], [0.349, 0.686, 0.168], [0.393, 0.769, 0.189]]))
+    sepiaFilter = np.array(frame, dtype = np.float64)
+    # apply a transformation where we multiply each pixel channel
+    # with the matrix for the sepia effect
+    sepiaFilter = cv2.transform(sepiaFilter, np.matrix([[0.272, 0.543, 0.131], 
+                                                        [0.349, 0.686, 0.168], 
+                                                        [0.393, 0.769, 0.189]]))
+    # normalize values greater than 255 to 255
     sepiaFilter[np.where(sepiaFilter > 255)] = 255
     sepiaFilter = np.array(sepiaFilter, dtype = np.uint8)
     return sepiaFilter
 
+def cartoonFilter (frame):
+    # Transform the image
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.medianBlur(gray, 5)
+    edges = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 9, 9)
+    color = cv2.bilateralFilter(frame, 9, 250, 250)
+    cartoon = cv2.bitwise_and(color, color, mask=edges)
+    return cartoon
+
 def main():
-    sung_glass = cv2.imread('sung.png', cv2.IMREAD_UNCHANGED)
     camera = cv2.VideoCapture(0) 
     face_locations = []
-    sung = cv2.imread('sung.png', cv2.IMREAD_UNCHANGED)
+    
     while True:
         ret, frame = camera.read()    #Capture frame
         face_locations = face_recognition.face_locations(frame) # Look for all faces in the camera
         #Draw a box around the face and show the results
         for top, right, bottom, left in face_locations:
             cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
-            sung = cvzone.overlayPNG(frame, sung_glass, [left,top])
-            cv2.imshow('filter2', sung)
-
+            
+        cartoon = cartoonFilter(frame)
+        cv2.imshow('filter2', cartoon)
+        
         sepia = sepiaFilter(frame)
         cv2.imshow('filter1', sepia)
         
